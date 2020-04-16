58bc4b3500291a4bf1865ffbea154315b1aed3cb
jargalbat <38040248+jargalbat@users.noreply.github.com>
2020-04-16 23:26:11 +0800

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_facebook/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,77 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginScreen(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool _isLoggedIn = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Facebook Login"),
-        ),
-        body: Container(
-          child: Center(
-            child: _isLoggedIn
-                ? Text("Logged In")
-                : RaisedButton(
-              child: Text("Login with Facebook"),
-              onPressed: () => initiateFacebookLogin(),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  void initiateFacebookLogin() async {
-    var facebookLogin = FacebookLogin();
-    var facebookLoginResult =
-    await facebookLogin.logIn(['public_profile']);
-    switch (facebookLoginResult.status) {
-      case FacebookLoginStatus.error:
-        print("Error");
-        onLoginStatusChanged(false);
-        break;
-      case FacebookLoginStatus.cancelledByUser:
-        print("CancelledByUser");
-        onLoginStatusChanged(false);
-        break;
-      case FacebookLoginStatus.loggedIn:
-//        print("LoggedIn");
-//
-//        var graphResponse = await http.get(
-//            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${facebookLoginResult
-//                .accessToken.token}');
-//
-//        var profile = json.decode(graphResponse.body);
-//        print(profile.toString());
-//
-//        onLoginStatusChanged(true, profileData: profile);
-        print("LoggedIn");
-        onLoginStatusChanged(true);
-        break;
-    }
-  }
-
-  void onLoginStatusChanged(bool isLoggedIn) {
-    setState(() {
-      this._isLoggedIn = isLoggedIn;
-    });
   }
 }
